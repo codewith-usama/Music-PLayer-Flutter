c7fa525e68568e50a54e655f7d1fa7c503bec656
Usama <epiicuree@gmail.com>
2023-07-11 15:27:41 +0500

    resouces added

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,35 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:music_player/Screens/playing_song.dart';
-import 'package:on_audio_query/on_audio_query.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:just_audio_background/just_audio_background.dart';
+import 'package:music_player/provider/song_model_provider.dart';
+import 'package:provider/provider.dart';
+import 'features/shared/ui/screens/songs_playing.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await JustAudioBackground.init(
+    androidNotificationChannelId: 'com.ryanheise.bg_demo.channel.audio',
+    androidNotificationChannelName: 'Audio playback',
+    androidNotificationOngoing: true,
+  );
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => SongModelProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Music Player',
-      theme: ThemeData.dark(),
-      home: const AllSongs(),
-    );
-  }
-}
-
-class AllSongs extends StatefulWidget {
-  const AllSongs({super.key});
-
-  @override
-  State<AllSongs> createState() => _AllSongsState();
-}
-
-class _AllSongsState extends State<AllSongs> {
-  final OnAudioQuery _audioQuery = OnAudioQuery();
-  final AudioPlayer _audioPlayer = AudioPlayer();
-
-  playSong(String? uri) {
-    try {
-      _audioPlayer.setAudioSource(AudioSource.uri(
-        Uri.parse(uri!),
-      ));
-      _audioPlayer.play();
-    } on Exception {
-      log("Error parsing song");
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Permission.storage.request();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Music PLayer'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-          ),
-        ],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
       ),
-      body: FutureBuilder<List<SongModel>>(
-        future: _audioQuery.querySongs(
-          sortType: null,
-          orderType: OrderType.ASC_OR_SMALLER,
-          uriType: UriType.EXTERNAL,
-          ignoreCase: true,
-        ),
-        builder: (context, item) {
-          if (item.data == null) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (item.data!.isEmpty) {
-            return const Center(
-              child: Text('No songs found'),
-            );
-          }
-          return ListView.builder(
-            itemCount: item.data!.length,
-            itemBuilder: (context, index) => ListTile(
-              leading: const CircleAvatar(child: Icon(Icons.music_note)),
-              title: Text(item.data![index].displayNameWOExt),
-              subtitle: Text(item.data![index].artist.toString()),
-              trailing: const Icon(Icons.more_horiz),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PlayingSong(
-                      songModel: item.data![index],
-                      audioPlayer: _audioPlayer,
-                    ),
-                  ),
-                );
-              },
-            ),
-          );
-        },
-      ),
+      home: const SongsPlaying(),
     );
   }
 }
